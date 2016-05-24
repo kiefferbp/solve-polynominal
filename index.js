817d7a0a2fd8027a91d3d32bcc95f5b84d1ce24c
kiefferbp <kiefferbp@gmail.com>
2016-05-24 14:32:34 -0500

    index.js: more work on the quartic subroutine

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-(function () {
+var solveQuartic = (function () {
     "use strict";
 
     // a helper function that returns the square of x
@@ -21,7 +21,34 @@
         if (x >= 0) {
             return Math.pow(x, 1/3);
         } else { // Math.pow attempts to compute a complex cube root for negative numbers
-            return -1*Math.pow(-x, 1/3); // use the identity (-x)^(1/3) = -x^(1/3) (for real numbers)
+            return -1*Math.pow(-x, 1/3); // use the identity x^(1/3) = -(-x)^(1/3) (for real numbers)
+        }
+    }
+
+    // a helper function that returns the square root of n
+    // n is either a real number or an object that represents the real and imaginary parts of n
+    // in the case that n is complex, we return its principal square root
+    function squareRoot(n) {
+        var r, x, y;
+
+        if (typeof n === "number") {
+            if (n >= 0) {
+                return Math.pow(n, 1/2);
+            } else {
+                throw new RangeError("x must either be a positive and real or a complex number.");
+            }
+        } else if (n !== null && typeof n === "object") {
+            if (n.imaginaryPart.isApproximatelyZero()) { // real number in object form
+                return {realPart: squareRoot(n.realPart), imaginaryPart: 0};
+            } else { // complex number
+                r = squareRoot(square(n.realPart) + square(n.imaginaryPart));
+                y = squareRoot((r - n.realPart)/2);
+                x = n.imaginaryPart/(2*y);
+
+                return {realPart: x, imaginaryPart: y};
+            }
+        } else {
+            throw new RangeError("x must either be a positive and real or a complex number.");
         }
     }
 
@@ -31,34 +58,10 @@
         return (Math.abs(this) < 1e-12);
     };
 
-    // a helper function that returns the square root of x
-    // x is either a real number or an object that represents the real and imaginary parts of x
-    function squareRoot(n) {
-        var r, x, y;
-        var squareRoot1 = {};
-        var squareRoot2 = {};
-
-        if (typeof n === "number") {
-            if (n >= 0) {
-                return Math.pow(n, 1/2);
-            } else {
-                throw new RangeError("x must either be a positive and real or a complex number.");
-            }
-        } else if (n !== null && typeof n === "object") {
-            r = squareRoot(square(n.realPart) + square(n.imaginaryPart));
-            y = squareRoot((r - a)/2);
-            x = b/(2*y);
-
-            squareRoot1.realPart = x;
-            squareRoot1.imaginaryPart = y;
-
-            squareRoot2.realPart = -x;
-            squareRoot2.imaginaryPart = -y;
-
-            return [squareRoot1, squareRoot2];
-        } else {
-            throw new RangeError("x must either be a positive and real or a complex number.");
-        }
+    // we also need the same function for objects that represent complex numbers
+    Object.prototype.isApproximatelyZero = function () {
+        var absoluteValue = squareRoot(square(this.realPart) + square(this.imaginaryPart));
+        return (absoluteValue < 1e-12);
     }
 
     // a helper function that solves the quadratic equation ax^2 + bx + c = 0
@@ -102,11 +105,11 @@
             solution2.isReal = false;
 
             solution3.realPart = -1*(S + U)/2 - b/(3*a);
-            solution3.imaginaryPart = (S - U)*squareRoot(3)/2;
+            solution3.imaginaryPart = -1*(S - U)*squareRoot(3)/2;
             solution3.isReal = false;
         } else { // 3 real solutions
             if (f.isApproximatelyZero() && g.isApproximatelyZero() && h.isApproximatelyZero()) { // all solutions are equal
-                solution1.realPart = solution2.realPart = solution3.realPart = Math.pow(d/a, 1/3);
+                solution1.realPart = solution2.realPart = solution3.realPart = cubeRoot(r/a);
                 solution1.imaginaryPart = solution2.imaginaryPart = solution3.imaginaryPart = 0;
                 solution1.isReal = solution2.isReal = solution3.isReal = true;
             } else { // unequal solutions
@@ -122,13 +125,13 @@
                 solution1.imaginaryPart = 0;
                 solution1.isReal = true;
 
-                solution1.realPart = L*(M + N) + P;
-                solution1.imaginaryPart = 0;
-                solution1.isReal = true;
+                solution2.realPart = L*(M + N) + P;
+                solution2.imaginaryPart = 0;
+                solution2.isReal = true;
 
-                solution1.realPart = L*(M - N) + P;
-                solution1.imaginaryPart = 0;
-                solution1.isReal = true;
+                solution3.realPart = L*(M - N) + P;
+                solution3.imaginaryPart = 0;
+                solution3.isReal = true;
             }
         }
 
@@ -149,12 +152,24 @@
     // a helper function that solves the quartic equation ax^4 + bx^3 + cx^2 + dx + e = 0
     // uses the method discribed at 1728.com
     function solveQuartic(a, b, c, d, e) {
+        debugger;
+
+        // quartic solutions
+        var solution1 = {};
+        var solution2 = {};
+        var solution3 = {};
+        var solution4 = {};
+        var solutions = [solution1, solution2, solution3, solution4];
+
+        // for intermediate calculations
+        var f, g, h, p, q, r, s, Y2, Y3;
+
         // normalize the coefficients
-        a = 1;
         b = b/a;
         c = c/a;
         d = d/a;
         e = e/a;
+        a = 1;
 
         // intermediate calculations
         var f = c - 3*square(b)/8;
@@ -164,8 +179,55 @@
         // solve the resolvent cubic
         var resolventCubicSolutions = solveCubic(1, f/2, (square(f) - 4*h)/16, -1*square(g)/64);
 
-        // more to come!
+        // Note: the source says to pick two roots of the resolvent cubic as follows:
+        // 1) if all three roots are real, pick any two non-zero ones
+        // 2) if there are two complex solutions, pick those (in this case, solutions 2 and 3 are complex)
+        if (resolventCubicSolutions[1].isReal) { // three real roots
+            resolventCubicSolutions.forEach(function (solution) {
+                if (!solution.isApproximatelyZero() && Y2 !== undefined && Y3 === undefined) {
+                    Y3 = solution;
+                }
+
+                if (!solution.isApproximatelyZero() && Y2 === undefined) {
+                    Y2 = solution;
+                }
+            });
+        } else { // one real, two complex roots
+            Y2 = resolventCubicSolutions[1];
+            Y3 = resolventCubicSolutions[2];
+        }
+
+        p = squareRoot(Y2);
+        q = squareRoot(Y3);
+        s = b/(4*a);
+
+        // if p and q are complex, they are conjugates of each other
+        // consequently, pq is real and equal to the square of their moduli
+        if (resolventCubicSolutions[1].isReal) { // three real roots (p and q are real)
+            r = -g/(8 * p.realPart * q.realPart);
+        } else { // p and q are complex
+            r = -g/(8 * squareRoot(square(p.realPart) + square(p.imaginaryPart)));
+        }
+
+        // compute the solutions
+        solution1.realPart = p.realPart + q.realPart + r - s;
+        solution1.imaginaryPart = p.imaginaryPart + q.imaginaryPart;
+        solution1.isReal = solution1.imaginaryPart.isApproximatelyZero();
+
+        solution2.realPart = p.realPart - q.realPart - r - s;
+        solution2.imaginaryPart = p.imaginaryPart - q.imaginaryPart;
+        solution2.isReal = solution2.imaginaryPart.isApproximatelyZero();
+
+        solution3.realPart = -1*p.realPart + q.realPart - r - s;
+        solution3.imaginaryPart = -1*p.imaginaryPart + q.imaginaryPart;
+        solution3.isReal = solution3.imaginaryPart.isApproximatelyZero();
+
+        solution4.realPart = -1*p.realPart - q.realPart + r - s;
+        solution4.imaginaryPart = -1*p.imaginaryPart - q.imaginaryPart;
+        solution4.isReal = solution4.imaginaryPart.isApproximatelyZero();
+
+        return solutions;
     }
 
-    return false;
+    return solveQuartic;
 }());
